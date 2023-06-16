4b9c95fbae3647b2cb10b0cb18478441ec366ba5
jesse cole <cole.j.jesse@gmail.com>
2023-06-15 19:13:19 -0700

    first

---
+++ src/components/About.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import image from '../img/tech02.jpg';
+import {git} from '../data';
+import { linkedIn } from '../data';
+import {twitt} from '../data';
+
+
+const About = () => {
+
+    // var sectionStyle = {
+    //     width: "100%",
+    //     height: "400px",
+    //     backgroundImage: "url(" + { Background } + ")"
+    //   };
+
+    return (
+        <div className="h-80">
+            <section id="about" className="h-full">
+            {/* container mx-auto flex py-20 md:flex-row flex-col justify-evenly */}
+                <div className="bg-indigo-950 pl-24 h-full mx-auto flex  md:flex-row flex-col justify-evenly">
+                    <div style={{flex:1}} className="flex-col h-full flex justify-evenly items-center">
+                        <div className="w-200 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 text-center justify-center">
+                        <h1 className="title-font text-2xl mb-4 font-medium text-white">
+                            Hey I'm Jesse
+                        </h1>
+                        <div className="flex h-18">
+                            <a
+                            href="#contact"
+                            className="h-4/6 shadow md:shadow-xlg text-black bg-white border-0 focus:outline-none font-bold inline-flex hover:bg-gray-700 hover:text-white rounded pb-8 pt-2 pl-5 pr-5">
+                            Work With Me
+                            </a>
+                            <a
+                            href="#projects"
+                            className="h-4/6 shadow md:shadow-xlg ml-4 inline-flex text-black bg-white border-0 focus:outline-none hover:bg-gray-700 hover:text-white rounded font-bold pb-8 pt-2 pl-5 pr-5">
+                            See My Past Work
+                            </a>
+                        </div>
+                        </div>
+                        <div className="pb-4 flex-row justify-start flex w-3/6">
+                            <div className="flex w-10 h-10 mr-5">
+                                <a href="https://github.com/jessecole1">
+                                    <img src={git} />
+                                </a>
+                            </div>
+                            <div className="flex w-10 h-10 mr-5">
+                                <a href="https://www.linkedin.com/in/jessejcole/">
+                                    <img src={linkedIn} />
+                                </a>
+                            </div>
+                            <div className="flex w-10 h-10 mr-5">
+                                <a href="https://twitter.com/jessecole_">
+                                    <img src={twitt} />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{backgroundImage:`url(${image})`, backgroundRepeat:"no-repeat", backgroundSize: "cover"}} className="bg-black h-full flex items-center justify-center lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                        <img style={{position: 'relative', right: 255}}
+                            className="object-cover object-center rounded w-64"
+                            alt="react"
+                            src="./logo.png"
+                        />
+                    </div>
+                </div>
+                <div className="flex items-center justify-end text-xs">
+                    <p>
+                    <a href="http://www.freepik.com">Designed by Freepik</a>
+                    </p>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/data.js
@@ -0,0 +1,55 @@
+// src/data.js
+
+export const projects = [
+    {
+      title: "Twitter Clone",
+      subtitle: "Java Spring",
+      description:
+        "Replication of Twitter with most of its features - Posting Tweets, Liking / Commenting, Notification Menu, Profile Page",
+      image: "./twitter.png",
+      link: "",
+    },
+    {
+      title: "2D Video Game",
+      subtitle: "Java Spring",
+      description:
+        "2D RPG Video Game with world map, 60FPS, character sprites, movement keys, map rendering and moving camera.",
+      image: "./2DGame01.png",
+      link: "",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+export const skills = [
+    "Python",
+    "Java",
+    "JavaScript",
+    "React.js",
+    "MERN",
+    "MySQL",
+    "MongoDB",
+    "Spring Tool Suite",
+    "Flask",
+    "HTML",
+    "CSS",
+    "Tailwind"
+]
+
+export const git = "./git.png";
+export const linkedIn = "./linkedin.png";
+export const twitt = "./twitt.png";
